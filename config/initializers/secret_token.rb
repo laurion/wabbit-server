27c5622799afc5491b164b7ca83b2b75e1b29bad
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2013-11-23 14:56:05 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WabbitServer::Application.config.secret_token = '86c8cfaa59fe15fa7bed8594c1f1a0a1f1158b686cd658c302445b207eba01af5633c7ac00f9e8d0446ae88eb6387598013cf82b0f7cd196630f612f3c8a664c'
