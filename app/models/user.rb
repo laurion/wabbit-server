ebc331771e7e1189fdadf57066cfbfa1d297884e
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2013-11-24 04:28:03 +0200

    unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	attr_accessible :google_id, :mac_addr, :token, :in_range
+
+	validates :google_id, :presence => true, :uniqueness => true
+	validates :mac_addr, :presence => true, :uniqueness => true
 end
