216566a40d9b416cca781c3bf439c63bafee2b71
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2013-11-24 02:40:56 +0200

    fix end

--- app/controllers/macs_controller.rb
+++ app/controllers/macs_controller.rb
@@ -11,43 +11,42 @@ class MacsController < ApplicationController
  #    end
 	# end
 
-	def create
-		p params
-		resp = JSON.parse request.body.read()
-		p resp
-		macs_in_range = resp["macs"]
-		piconet_id = resp["piconet_id"]
-		p macs_in_range
-		p "piconet_id: "
-		p piconet_id
+def create
+	p params
+	resp = JSON.parse request.body.read()
+	p resp
+	macs_in_range = resp["macs"]
+	piconet_id = resp["piconet_id"]
+	p macs_in_range
+	p "piconet_id: "
+	p piconet_id
 
-		works = true
-		err = nil
+	works = true
+	err = nil
 
-		User.all.each do |user|
-			if macs_in_range and piconet_id
-				if macs_in_range.include?(user.mac_addr)
-					user.piconet_id = piconet_id
-				else
-					user.piconet_id = nil
-				end
-			end
-			if not user.save
-				works = false
+	User.all.each do |user|
+		if macs_in_range and piconet_id
+			p piconet_id
+			if macs_in_range.include?(user.mac_addr)
+				user.piconet_id = piconet_id
 			else
-				err = user.errors
+				user.piconet_id = nil
 			end
 		end
+		if not user.save
+			works = false
+		else
+			err = user.errors
+		end
+	end
 
-		respond_to do |format|
-      if works
-        # format.html { redirect_to @user, notice: 'Macs were successfully received.' }
-        format.json { render json: resp, status: :created, location: resp }
-      else
-        # format.html { render action: "new" }
-        format.json { render json: err.errors, status: :unprocessable_entity }
-      end
+	respond_to do |format|
+    if works
+      # format.html { redirect_to @user, notice: 'Macs were successfully received.' }
+      format.json { render json: resp, status: :created, location: resp }
+    else
+      # format.html { render action: "new" }
+      format.json { render json: err.errors, status: :unprocessable_entity }
     end
-
 	end
 end
